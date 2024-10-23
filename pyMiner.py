eb4b28217ee6ef84304a30531a532ceffd96038d
tokimay <tokimay@gmail.com>
2024-10-23 15:12:05 +0330

    edit 1

--- pyMiner.py
+++ pyMiner.py
@@ -1,35 +1,44 @@
+import datetime
 import random
 
-from bitcoinCore import bitcoinCore, calculation
+from bitcoin import bitcoinCore, calculation
 
+
+isRegTest = True
 user = 'user'  # change with your own
 password = 'userpass'  # change with your own
+myAddress = 'bitcoinAddress'  # change with your own
 
-# for RegTest
-core = bitcoinCore(user, password, "http://127.0.0.1", 18443)
-# for MainNet
-# core = bitcoinCore(user, password, "http://127.0.0.1", 8332)
+if isRegTest:
+    core = bitcoinCore(user=user, password=password, host="http://127.0.0.1", port=18443)
+else:
+    core = bitcoinCore(user=user, password=password, host="http://127.0.0.1", port=8332)
 
-#
+
 """ if your address is generated in bitcoinCore """
-myAddress = 'Your bitcoin address'  # change with your own
 addressInfo = core.getAddressInfo(myAddress)
 myPubKey = addressInfo['result']['pubkey']
 """ if address is generated in third party wallet """
 # myPubKey = "insert your pubKey"
 
 newExtraNonce = True
+nonce = 0
+refreshThreshold = 100_000_000
+
 while True:
     if newExtraNonce:
         nonce = 0
         extraNonce = random.randint(0x0, 0xffff)
         newExtraNonce = False
+        refreshThreshold = 100_000_000
         print('extraNonce', extraNonce)
     tempBlock = core.getBlockTemplate()
+    print('block template refreshed')
     target = int(tempBlock['result']['target'], 16)
 
     # if you are on RegTest make it a little harder
-    target = int(target/pow(2, 24))
+    if isRegTest:
+        target = int(target / pow(2, 24))
 
     ver = (tempBlock['result']['version'].to_bytes((tempBlock['result']['version'].bit_length() + 7) // 8, 'little')).hex()
     previousBlockhash = calculation.reverse(tempBlock['result']['previousblockhash'])
@@ -58,15 +67,19 @@ while True:
     merkleRoot = calculation.reverse(calculation.merkleRoot(branchList=merkleBranch))
     nTimeNew = calculation.reverse((hex(tempBlock['result']['curtime']))[2:])
     lenNounce = int(len(tempBlock['result']['noncerange']) / 2)
-    #nonceRange = 10_000_000
+    print('start checking nonce range:')
     while nonce <= nonceRange:
         nonce += 1
         blockHeader = ver + previousBlockhash + merkleRoot + nTimeNew + bits + (hex(nonce)[2:]).zfill(8)
         blockRaw = blockHeader + lenTransactions + transactionsRaw
         solution = calculation.headerHash(blockHeader)
-        if nonce % 1_000_000 == 0:
-            print(f"{int(nonce/1_000_000)} mega nonce number checked")
-            break  # exit loop for change re-fetch block template
+        if nonce % 1_000_000 == 0 and nonce != 0:
+            print(f"{int(nonce/1_000_000)} mega nonce number checked {datetime.datetime.now()}")
+            if nonce % refreshThreshold == 0 and nonce != 0:
+                nonce += 1
+                refreshThreshold += 100_000_000
+                print('going to re-fetch block template')
+                break  # exit loop to get new block template
         if int(solution, 16) < target:
             block = core.submitBlock(blockRaw)
             if block['error'] is None:
@@ -74,6 +87,6 @@ while True:
                 print(f"succeed \nheight  : {b['result']['blocks']} \nhash    :{b['result']['bestblockhash']}")
                 exit(0)
     if nonce >= nonceRange:
-        print('all nonce range checked.\ntry new extra nounce to change coinbase hash -> merkle root -> header hash')
+        print(
+            'all nonce range checked.\ntry new extra nounce to change coinbase hash -> merkle root hash -> header hash')
         newExtraNonce = True
-
